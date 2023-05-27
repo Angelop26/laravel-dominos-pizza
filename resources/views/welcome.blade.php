345a5f3b650716a29ac194d3fb3629db465fb8b2
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-05-27 11:48:38 +0200

    added welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,24 +2,17 @@
 
 {{-- page name --}}
 @section('page-name')
-    Our Pizzas
+    Welcome
 @endsection
 @section('content')
     <main>
+        <div class="jumbotron">
+
+        </div>
         <div class="container">
-            <div class="row row-cols-4">
-                @foreach ($pizzas as $pizza)
-                    <div class="col">
-                        <div class="card mb-4">
-                            <div class="card-body">
-                                <h5 class="card-title">Pizza {{ $pizza->name }}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">{{ $pizza->formato }}</h6>
-                                <p class="card-text"></p>
-                                <a href="#" class="btn btn-primary">Card link</a>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
+            <div class="ms_container d-flex flex-column align-items-center">
+                <h1 class="text-center"> Dai un occhiata alle nostre pizze</h1>
+                <a class="btn btn-secondary " href="">Mostra Pizze</a>
             </div>
         </div>
     </main>
